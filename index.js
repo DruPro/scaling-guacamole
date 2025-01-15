a6de3e577736c1413777566255e9293d275b032c
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-01-15 14:23:07 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const port = 3001;
+const cors = require('cors');
+
+app.use(cors())
+app.get('/gem',(req,res)=>{
+    res.send(JSON.stringify({Gem: 1}))
+})
+
+app.get('/message',(req,res)=>{
+    res.send(JSON.stringify({Message: "Arrr matey! The servers be sailin' the digital seas. Beware the cyber waves, for they be as treacherous as a pirate’s stormy path! Only those with the proper key can unlock the treasure hidden in the data chests!"}))
+})
+
+app.listen(port,()=>{console.log('Server Started')})
