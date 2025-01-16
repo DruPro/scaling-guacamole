4283b84fdf8c23874b2ebbebe8bfa017f0e1b207
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-01-15 21:02:55 -0800

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,12 +7,19 @@ app.use(cors({
     origin: '*'
   }));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/gem',(req,res)=>{
+    console.log('Received request for /gem');
     res.send(JSON.stringify({Gem: 1}))
 })
 
 app.get('/message',(req,res)=>{
+    console.log('Received request for /message');
     res.send(JSON.stringify({Message: "Arrr matey! The servers be sailin' the digital seas. Beware the cyber waves, for they be as treacherous as a pirate’s stormy path! Only those with the proper key can unlock the treasure hidden in the data chests!"}))
 })
 
-app.listen(port,()=>{console.log('Server Started')})
+app.listen(port,()=>{console.log(`Server Initilized | Port : ${port}`)})
